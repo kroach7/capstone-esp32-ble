adea5d72ff137b65f34817ad11e6b118dc707a32
KRoach <kyle_roach7@outlook.com>
2023-11-14 13:40:20 -0500

    upddate to handle temp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define LED 2
+#define TEMP_BUS 33
+#define RELAY_PIN 32
+
+// Setup a oneWire instance to communicate with any Temp device
+OneWire oneWire(TEMP_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
 
 // Define the service and characteristic UUIDs
 BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-BLEUUID characteristicUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+BLEUUID tempCharacteristicUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+BLEUUID relayCharacteristicUUID("26e2b12b-85f0-4f3f-916f-5d0af7256323");
 
 // Create a BLE server
 BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
+BLECharacteristic *pTempCharacteristic = NULL;
+BLECharacteristic *pRelayCharacteristic = NULL;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -20,16 +31,33 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
     void onDisconnect(BLEServer* pServer) {
       Serial.println("Client disconnected");
-      pServer->startAdvertising();
+      pServer->getAdvertising()->start();
+    }
+};
+
+
+class RelayCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+        Serial.print("Received over BLE: ");
+        Serial.println(value.c_str());
+        
+        if (value[0] == '1') {
+          digitalWrite(RELAY_PIN, HIGH);
+        } else if (value[0] == '0') {
+          digitalWrite(RELAY_PIN, LOW);
+        }
+      }
     }
 };
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED, OUTPUT);
 
   // Create the BLE device
-  BLEDevice::init("ESP32_LED");
+  BLEDevice::init("ESP32_SMART_LIVEWELL");
 
   // Create the BLE server
   pServer = BLEDevice::createServer();
@@ -38,14 +66,18 @@ void setup() {
   // Create the BLE service
   BLEService *pService = pServer->createService(serviceUUID);
 
-  // Create the BLE characteristic
-  pCharacteristic = pService->createCharacteristic(
-    characteristicUUID,
+  pTempCharacteristic = pService->createCharacteristic(
+    tempCharacteristicUUID,
     BLECharacteristic::PROPERTY_READ |
-    BLECharacteristic::PROPERTY_WRITE |
-    BLECharacteristic::PROPERTY_NOTIFY |
-    BLECharacteristic::PROPERTY_INDICATE
+    BLECharacteristic::PROPERTY_NOTIFY
   );
+
+  // Relay control characteristic
+  pRelayCharacteristic = pService->createCharacteristic(
+    relayCharacteristicUUID,
+    BLECharacteristic::PROPERTY_WRITE
+  );
+  pRelayCharacteristic->setCallbacks(new RelayCharacteristicCallbacks());
 
   // Start the service
   pService->start();
@@ -57,15 +89,20 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  pCharacteristic->setValue("LED is on");
-  pCharacteristic->notify();
-  delay(1000);
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  float temperature = sensors.getTempCByIndex(0); // Get temperature in Celsius
+  
+  // Convert temperature to a string
+  char tempString[10];
+  dtostrf(temperature, 1, 2, tempString);
 
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  pCharacteristic->setValue("LED is off");
-  pCharacteristic->notify();
-  delay(1000);
+  Serial.print("Temperature: ");
+  Serial.print(tempString);
+  Serial.println(" °C");
+
+  // Send temperature over BLE
+  pTempCharacteristic->setValue(tempString);
+  pTempCharacteristic->notify();
+
+  delay(1000); 
 }
